1af992ffed922e0187a1620ae0663a927a76e000
Daniel Silva <danielsilva@mail.weber.edu>
2020-05-04 14:31:20 -0600

    Refactored route requires.

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
-const app = express();
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/public', express.static('public'));
-app.set('view engine', 'pug');
-
 //require routes
 const indexRoute = require('./routes');
 const cardRoute = require('./routes/cards');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/public', express.static('public'));
+app.set('view engine', 'pug');
 
 //call routes
 app.use(indexRoute);
@@ -35,7 +34,5 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-//create server
-app.listen(8080, () => {
-    console.log('Flashcard App Server Running');
-});
+
+module.exports = app;

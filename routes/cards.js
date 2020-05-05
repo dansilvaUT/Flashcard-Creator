6e9b53d2c580e963d0683d1d3407507679a72aa1
Daniel Silva <danielsilva@mail.weber.edu>
2020-05-04 21:48:04 -0600

    Added styles to cards and headers.

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//const Flashcard = require('../models').Flashcard;
+const Flashcard = require('../models').Flashcard;
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb) {
@@ -13,30 +13,33 @@ function asyncHandler(cb) {
     }
 }
 
-router.get("/", (req, res) => {
-    res.render('cards', { title: 'Flashcards' });
+//Get All Cards
+router.get('/', asyncHandler(async (req, res) => {
+    let cards = await Flashcard.findAll();
+    res.render('cards', { cards, title: 'Flashcards' });
+}));
+
+//Add New Card Form
+router.get('/new', (req, res) => {
+    res.render('addcard', { title: 'Add New Flahscards' });
 });
 
-//Get card based on id
-router.get("/:id", (req, res) => {
-    let { side } = req.query;
-    const { id } = req.params;
-    if (!side) {
-        res.redirect(`/cards/${id}?side=question`)
-    }
-    const question = cards[id][side];
-    const { hint } = cards[id];
-    const templateData = { id, question };
-    if (side === 'question') {
-        templateData.hint = hint;
-        templateData.sideToShow = 'answer';
-        templateData.sideToShowDisplay = 'Answer';
-    } else if (side === 'answer') {
-        templateData.sideToShow = 'question';
-        templateData.sideToShowDisplay = 'Question';
-    }
-
-    res.render("cards", templateData);
-});
+//Post a new question
+router.post('/', asyncHandler(async (req, res) => {
+    const card = await Flashcard.create(req.body);
+    res.redirect('addcard');
+    // let card;
+    // try {
+    //     card = await Flashcard.create(req.body);
+    //     res.redirect("/cards/" + card.id);
+    // } catch (error) {
+    //     if (error.name === "SequelizeValidationError") {
+    //         card = await Flashcard.build(req.body);
+    //         res.render("card", { card, errors: error.errors, title: 'Flashcards' })
+    //     } else {
+    //         throw error;
+    //     }
+    // }
+}));
 
 module.exports = router;

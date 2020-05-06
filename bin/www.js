48352a05ce98a0f3c8cda304181e60cc96faa8cd
Daniel Silva <danielsilva@mail.weber.edu>
2020-05-05 20:01:49 -0600

    Added console log function to display port.

--- bin/www.js
+++ bin/www.js
@@ -28,6 +28,7 @@ var server = http.createServer(app);
 
 sequelize.sync().then(() => {
   server.listen(port);
+  console.log('Flashcard App listening on port: ', port);
 });
 server.on('error', onError);
 server.on('listening', onListening);

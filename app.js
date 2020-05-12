987fb1da5425a90db2ee635f509245bf6e2abec0
Daniel Silva <danielsilva@mail.weber.edu>
2020-05-11 21:50:08 -0600

    Added session and bcrypt.

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 //require routes
 const indexRoute = require('./routes');
 const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const cardRoute = require('./routes/cards');
+const profileRoute = require('./routes/profile');
 const app = express();
+
+app.use(session({
+    secret: 'this app is aignt',
+    resave: true,
+    saveUninitialized: false
+}));
 
 //Connect to DB
 mongoose.connect("mongodb://localhost:27017/flashcardflashstudy", {
@@ -32,6 +41,7 @@ app.use(indexRoute);
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
 app.use('/cards', cardRoute);
+app.use('/profile', profileRoute);
 
 //middleware
 app.use((req, res, next) => {

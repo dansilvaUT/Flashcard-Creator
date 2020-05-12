9211e5ef36ee7f83d7365f5d230bccd733c4c16c
Daniel Silva <danielsilva@mail.weber.edu>
2020-05-11 21:50:44 -0600

    Added authentication on users model.

--- models/users.js
+++ models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -17,5 +19,36 @@ const UserSchema = new mongoose.Schema({
     }
 }, { timestamps: { createdAt: 'created_at' } });
 
-const user = mongoose.model('User', UserSchema);
-module.exports = user;
+UserSchema.statics.authenticate = (email, password, cb) => {
+    User.findOne({ email: email })
+        .exec((error, user) => {
+            if (error) {
+                cb(error);
+            } else if (!user) {
+                const error = new Error("User not found");
+                error.status = 401;
+                cb(error);
+            }
+            bcrypt.compare(password, user.password, function (error, result) {
+                if (result === true) {
+                    cb(null, user);
+                } else {
+                    cb();
+                }
+            });
+        })
+}
+
+UserSchema.pre('save', function (next) {
+    let user = this;
+    bcrypt.hash(user.password, 10, function (error, hash) {
+        if (error) {
+            console.log("Error:", error);
+        } else {
+            user.password = hash;
+            next();
+        }
+    });
+});
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
